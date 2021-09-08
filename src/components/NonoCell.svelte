d03730faa1f2337579b42aedae35e1f034a93dfb
Brooks Van Buren <mail@brooks.codes>
2021-09-08 13:27:18 -0700

    Fix cell state classes

--- src/components/NonoCell.svelte
+++ src/components/NonoCell.svelte
@@ -2,8 +2,10 @@
     import { GridCell, CellState } from "/src/Game";
 
     export let gridCell: GridCell;
+
+
 </script>
-<div data-x={gridCell.x} data-y={gridCell.y} class:marked="{gridCell.state === CellState.marked}" class:crossed="{gridCell.state === CellState.Crossed}">
+<div data-x={gridCell.x} data-y={gridCell.y} class:marked={gridCell.state === CellState.Marked} class:crossed={gridCell.state === CellState.Crossed}>
 
     x: {gridCell.x} y: {gridCell.y}
     <br>
