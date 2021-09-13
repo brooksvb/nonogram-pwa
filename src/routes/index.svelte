773f7d40c35954ea2221df2b269fe5e272722fa9
Brooks Van Buren <mail@brooks.codes>
2021-09-13 16:20:12 -0700

    Adjust styling of main menus

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,17 +5,26 @@
 
 <main class="text-center mt-32">
 	<h1 class="text-4xl">NonoGame</h1>
-	<p>Welcome to NonoGame</p>
+	<p class="mb-4">Welcome to NonoGame</p>
 
-	<div class="bg-gray-300 inline-block p-8">
-		<h4>Set size of grid</h4>
-		<label for="rows">Rows</label>
-		<input name="rows" type="number" min="5" max="15" bind:value={rows}>
-		<label for="cols">Columns</label>
-		<input name="cols" type="number" min="5" max="15" bind:value={cols}>
+	<div class="inline-block bg-gray-300 p-8">
+		<h4>Set size of grid and click Play</h4>
 
-		<a sveltekit:prefetch href="game?rows={rows}&cols={cols}"
-		class="inline-block mt-16 p-8 bg-yellow-500 text-2xl rounded-md active:filter active:brightness-[.85]"
-		>Play Game</a>
+		<div class="flex justify-center items-center mt-4">
+			<div class="inline-flex flex-col items-end mr-4">
+				<div class="mb-2">
+					<label for="rows">Rows</label>
+					<input name="rows" type="number" min="5" max="15" bind:value={rows}>
+				</div>
+				<div>
+					<label for="cols">Columns</label>
+					<input name="cols" type="number" min="5" max="15" bind:value={cols}>
+				</div>
+			</div>
+	
+			<a sveltekit:prefetch href="game?rows={rows}&cols={cols}"
+			class="inline-block p-8 bg-yellow-500 text-2xl rounded-md active:filter active:brightness-[.85]"
+			>Play Game</a>
+		</div>
 	</div>
 </main>
