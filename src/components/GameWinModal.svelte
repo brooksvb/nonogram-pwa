4bf574172373ced631c519d5b2ea1a29f9e77465
Brooks Van Buren <mail@brooks.codes>
2021-09-24 19:38:11 -0700

    Increase button text size

--- src/components/GameWinModal.svelte
+++ src/components/GameWinModal.svelte
@@ -13,8 +13,8 @@
 	<h1 class="text-4xl mb-4">You completed this nonogram!</h1>
 	<p class="text-2xl">Your completion time: <span class="text-green-500">{completionTimeString}</span></p>
 
-	<a href="." on:click="{() => {location.reload(); return false;}}" class="border-green-500 bg-green-300 rounded-md">Play Again</a>
-	<a href="/" class="border-blue-500 bg-blue-300 rounded-md">Return to Main Menu</a>
+	<a href="." on:click="{() => {location.reload(); return false;}}" class="text-xl border-green-500 bg-green-300 rounded-md">Play Again</a>
+	<a href="/" class="text-xl border-blue-500 bg-blue-300 rounded-md">Return to Main Menu</a>
 </div>
 
 
