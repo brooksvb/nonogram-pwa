ccdb31e86d9d2d78d377d7a3f3d95544e1fd9add
Brooks Van Buren <mail@brooks.codes>
2021-11-06 14:32:33 -0500

    Extract GridHelper into file

--- src/Game.ts
+++ src/Game.ts
@@ -1,5 +1,6 @@
 import { gridStore, timerStore } from "./stores";
 import { get as get_store_value } from 'svelte/store';
+import { GridHelper } from "./GridHelper";
 
 /**
  * The GridData matrix has it's origin on the bottom left. First coordinate (x) refers to the column, 
@@ -30,6 +31,17 @@ export enum SelectionMode {
     Crossing
 }
 
+export interface GridSelection {
+    startCoord: {
+        x: number,
+        y: number
+    },
+    endCoord: {
+        x: number,
+        y: number
+    },
+    valid: boolean
+}
 
 export class GameController {
 
@@ -155,105 +167,3 @@ export class GameController {
         return !hasError;
     }
 }
-
-export class GridHelper {
-
-    public static generateGrid(width: number, height: number): GridData {
-        // Initializing typed multidim. array: https://stackoverflow.com/a/47801159/5869958
-        const grid = new Array<Array<GridCell>>();
-        for (let x = 0; x < width; x++) {
-            const col: GridCell[] = new Array<GridCell>();
-            for (let y = 0; y < height; y++) {
-                const isTarget = Math.random() >= .5;
-                col[y] = {
-                    state: CellState.Empty,
-                    target: isTarget, // Solution state of cell
-                    x: x,
-                    y: y
-                };
-            }
-            grid.push(col);
-        }
-        return grid;
-    }
-
-    public static coordsAreInSelection(x: number, y: number, selection: GridSelection): boolean {
-        const [lowerX, upperX] = selection.startCoord.x > selection.endCoord.x 
-        ? [selection.endCoord.x, selection.startCoord.x] 
-        : [selection.startCoord.x, selection.endCoord.x];
-        const [lowerY, upperY] = selection.startCoord.y > selection.endCoord.y
-        ? [selection.endCoord.y, selection.startCoord.y] 
-        : [selection.startCoord.y, selection.endCoord.y];
-        
-        return x >= lowerX && x <= upperX && y >= lowerY && y <= upperY;
-    }
-
-    /**
-     * 
-     * @param row 
-     * @returns array of numbers representing groups in the row
-     */
-    public static getRowGroups(row: number): number[] {
-        const grid = get_store_value(gridStore);
-        const groups = [];
-        let counter = 0;
-        for (let i = 0; i < grid.length; i++) {
-            if (grid[i][row].target) {
-                counter++;
-            } else if (counter !== 0) {
-                // Add the group number and restart count
-                groups.push(counter);
-                counter = 0;
-            }
-        }
-        if (counter !== 0) {
-            groups.push(counter);
-        }
-        return groups;
-    }
-
-    public static getColGroups(col: number): number[] {
-        const grid = get_store_value(gridStore);
-        const groups = [];
-        let counter = 0;
-        // Iterate backwards because headings read top-down
-        for (let i = grid[0].length - 1; i >= 0; i--) {
-            if (grid[col][i].target) {
-                counter++;
-            } else if (counter !== 0) {
-                // Add the group number and restart count
-                groups.push(counter);
-                counter = 0;
-            }
-        }
-        if (counter !== 0) {
-            groups.push(counter);
-        }
-        return groups;
-    }
-
-    public static printGridSolution(): void {
-        const grid = get_store_value(gridStore); 
-        let solution = '';
-        for (let y = grid[0].length - 1; y >= 0; y--) {
-            for (let x = 0; x < grid.length; x++) {
-                solution += grid[x][y].target ? 'X ' : 'o ';
-            }
-            solution += '\n';
-        }
-        console.log(solution);
-    };
-}
-
-export interface GridSelection {
-    startCoord: {
-        x: number,
-        y: number
-    },
-    endCoord: {
-        x: number,
-        y: number
-    },
-    valid: boolean
-}
-
